388772b5e0271e14cb60d85bb86c0488455bed74
tanph4t <pdtanphat3805@gmail.com>
2026-04-15 20:16:45 +0700

    feat: xuat du lieu dang JSON

---
+++ firmware/LM35_TempReader.ino
@@ -0,0 +1,22 @@
+int adcValues[2];
+float nhietDo[2];
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  adcValues[0] = analogRead(A0);
+  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
+
+  adcValues[1] = analogRead(A1);
+  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
+
+  Serial.print("{\"A0\":");
+  Serial.print((int)nhietDo[0]);
+  Serial.print(",\"A1\":");
+  Serial.print((int)nhietDo[1]);
+  Serial.println("}");
+
+  delay(100);
+}
